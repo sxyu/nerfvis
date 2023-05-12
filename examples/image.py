7742837b91ff1c8649fc0eb78f8ec124bc60105a
Alex Yu <alexyu99126@gmail.com>
2023-05-12 13:19:01 -0700

    Added image for example

--- examples/image.py
+++ examples/image.py
@@ -1,9 +1,10 @@
+# Example to add an image
 from nerfvis import scene
 import numpy as np
-import imageio
+import imageio.v2 as imageio
 
 bulldozer = imageio.imread('bulldozer.jpg')
-scene.add_cube('My cube/0', translation=[2,0,0])
+scene.add_cube('My cube/0', translation=[0,0,0])
 scene.add_image('My bulldozer image',
                 bulldozer,
                 r = np.eye(3),
